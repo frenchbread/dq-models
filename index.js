08acbd99554715bf824ef15c5bf1d50e075f6625
Damir Mustafin <frenchthebread@gmail.com>
2015-12-02 00:29:37 +0900

    models

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var Models = require('./lib/');
 
+/*
+ Models available:
+ 1. Account (mongoose, passportLocalMongoose)
+ 2. Article (mongoose)
+ 3. Project (mongoose)
+ 4. Watcher (mongoose)
+ 5. Task (mongoose)
+ 6. Hour (mongoose)
+ 7. Booking (mongoose)
+ */
 
-module.exports = require('./models')();
+module.exports = Models;
